1c6603146e02c2e3f5d122fabf5ae0089e274633
demola malomo <malomo.alaba@yahoo.com>
2021-11-12 16:50:07 +0100

    project setup and ui starter

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,59 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div class="">
+    <header
+      class="h-16 w-full bg-indigo-200 px-6 flex justify-between items-center"
+    >
+      <h1 class="text-xl text-indigo-900">React-Realm</h1>
+      <button
+        class="text-lg text-white capitalize px-6 py-2 bg-indigo-900 rounded-md"
+      >
+        create
+      </button>
+    </header>
+    <section className="mt-10 flex justify-center px-6">
+      <ul className="w-full">
+        <li className="border-2 p-6 rounded-lg flex items-center">
+          <section
+            className="h-10 w-10 bg-indigo-100 rounded-md flex justify-center items-center mr-4"
+          >
+            <UserIcon />
+          </section>
+          <section className="">
+            <h2 className="capitalize font-semibold mb-1">daniel mark</h2>
+            <p className="capitalize text-gray-500 mb-1">new york</p>
+            <p className="capitalize text-indigo-500 font-medium text-sm mb-2">
+              software engineer
+            </p>
+            <div className="flex">
+              <button
+                className="text-sm text-red-500 capitalize px-4 py-2 mr-4 border border-red-500 rounded-md"
+              >
+                delete
+              </button>
+              <button
+                className="text-sm text-white capitalize px-4 py-2 bg-indigo-900 rounded-md"
+              >
+                edit
+              </button>
+            </div>
+          </section>
+        </li>
+      </ul>
+    </section>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import { defineComponent } from 'vue';
+import UserIcon from '@/assets/svg/UserIcon.vue';
 
 export default defineComponent({
-  name: "App",
+  name: 'App',
   components: {
-    HelloWorld,
+    UserIcon
   },
+  
 });
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,72 @@
+<template>
+  <div
+    class="h-screen w-screen bg-indigo-900 bg-opacity-30 z-30 top-0 fixed transform scale-105 transition-all ease-in-out duration-100"
+  >
+    <div class="flex flex-col justify-center items-center h-full w-full open-nav">
+      <div class="flex justify-end w-11/12 lg:w-[479px] 2xl:w-6/12">
+        <div
+          role="button"
+          class="cursor-pointer w-6 h-6 rounded-full flex items-center justify-center bg-white"
+        >
+          <CloseIcon />
+        </div>
+      </div>
+      <section
+        class="w-11/12 lg:w-[479px] 2xl:w-6/12 bg-white flex justify-center items-center mt-5 rounded-lg"
+      >
+        <div class="w-11/12 py-8">
+          <h2 class="capitalize text-xl text-gray-500 font-medium mb-4">
+            <!-- {{isEdit ? 'Edit User' : 'add user'}} -->
+          </h2>
+          <form onsubmit="{handleSubmit}">
+            <fieldset class="mb-4">
+              <label class="block text-sm text-gray-500 capitalize mb-1">name</label>
+              <input
+                type="text"
+                name="name"
+                value="{value.name}"
+                onchange="{handleChange}"
+                required
+                class="w-full h-10 border border-gray-500 rounded-sm px-4"
+              />
+            </fieldset>
+            <fieldset class="mb-4">
+              <label class="block text-sm text-gray-500 capitalize mb-1">location</label>
+              <input
+                type="text"
+                name="location"
+                value="{value.location}"
+                onchange="{handleChange}"
+                required
+                class="w-full h-10 border border-gray-500 rounded-sm px-4"
+              />
+            </fieldset>
+            <fieldset class="mb-4">
+              <label class="block text-sm text-gray-500 capitalize mb-1">title</label>
+              <input
+                type="text"
+                name="title"
+                value="{value.title}"
+                onchange="{handleChange}"
+                required
+                class="w-full h-10 border border-gray-500 rounded-sm px-4"
+              />
+            </fieldset>
+            <button class="text-white capitalize px-6 py-2 bg-indigo-900 rounded-md w-full">save</button>
+          </form>
+        </div>
+      </section>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Modal",
+  props: {
+    msg: String,
+  },
+});
+</script>
